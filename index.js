6f4e6d69577c446997e3e4136ba20b52538a163d
wilberchair <wilberchair@gmail.com>
2021-11-01 19:25:22 -0300

    criando metodos

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ class Cliente {
   //esses são atributos, ou propriedades (fora de classes conhecemos como variáveis)
   nome;
   cpf;
+}
+
+class ContaCorrente {
   agencia;
-  saldo
+  saldo;
+
+  sacar(valor) {
+    if(this.saldo >= valor) {
+      this.saldo -= valor;
+    }
+  }
 }
 
 //Se eu quero criar um novo cliente eu utilizo a palavra new, gerando uma instância desta classe!
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = 'Ricardo';
 cliente1.cpf = '11122233309';
-cliente1.agencia = 1001; 
-cliente1.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 0;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo += 1000;
+console.log(contaCorrenteRicardo.saldo);
+let valorSacado = 1200;
+contaCorrenteRicardo.sacar(valorSacado);
+console.log(contaCorrenteRicardo.saldo);
+
+const cliente2 = new Cliente();
 cliente2.nome = 'Alice';
 cliente2.cpf = '88822233309';
-cliente2.agencia = 1001; 
-cliente2.saldo = 0;
 
 console.log(cliente1, cliente2);
