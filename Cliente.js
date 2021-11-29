03c26d42ff97b36c8cfc5f597376206f42a739b2
wilberchair <wilberchair@gmail.com>
2021-11-28 22:20:23 -0300

    finalizando curso de oo parte 1

--- Cliente.js
+++ Cliente.js
@@ -1,5 +1,16 @@
 export class Cliente {
   //esses são atributos, ou propriedades (fora de classes conhecemos como variáveis)
   nome;
-  cpf;
+  _cpf;
+
+  get cpf() {
+    return this._cpf;
+  }
+
+  //O construtor serve para nos ajudar a passar os parametros na chamada da classe na execução dela
+  //Ex new Cliente("Wilber", 35531875819)
+  constructor(nome, cpf) {
+    this.nome = nome;
+    this._cpf = cpf;
+  }
 }
--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-
 //Se eu quero criar um novo cliente eu utilizo a palavra new, gerando uma instância desta classe!
-const cliente1 = new Cliente();
-cliente1.nome = 'Ricardo';
-cliente1.cpf = '11122233309';
+const cliente1 = new Cliente('Ricardo', 11122233309)
 
-const cliente2 = new Cliente();
-cliente2.nome = 'Alice';
-cliente2.cpf = '88822233309';
+const cliente2 = new Cliente('Alice', 88822233309);
+console.log("conta2", cliente2);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 1002
+const conta2 = new ContaCorrente(1002, cliente2);
 
 contaCorrenteRicardo.transferir(200, conta2);
-console.log(contaCorrenteRicardo, conta2);
+console.log(ContaCorrente.numeroDeContas)
